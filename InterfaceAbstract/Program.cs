af19ae613cbc589eba3a301669985a351058aa11
Kinara <91768638+kinarajv@users.noreply.github.com>
2023-07-06 08:47:12 +0700

    first commit

---
+++ InterfaceAbstract/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace AnimalProgram;
+public interface IAnimal
+{
+    void Eat();
+}
+
+public interface IDog : IAnimal
+{
+    void Bark();
+    void Jump();
+}
+public abstract class Dog : IDog
+{
+    public void Bark()
+    {
+        Console.WriteLine("Bark");
+    }
+    public virtual void Eat()
+    {
+        Console.WriteLine("Dog Eat");
+    }
+    public abstract void Jump();
+}
+
+public class Poodle : Dog {
+    public override void Jump() {
+        Console.WriteLine("Poodle Jump");
+    }
+    public new static void Eat() {
+        Console.WriteLine("Poodle Eat");
+    }
+}
+
+static class Program {
+    static void Main() {
+        Poodle poodle = new();
+        Poodle.Eat();
+        poodle.Bark();
+        poodle.Jump();
+    }
+}
